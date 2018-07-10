b8fe0326174deb5c88472e41602e77990ef504a0
Matthew <tsengmatthew@hotmail.com>
2018-07-09 21:50:49 -0700

    fixed up nodejs server and readme

--- webapp/server.js
+++ webapp/server.js
@@ -1,14 +1,53 @@
-const express = require('express')
-const app = express()
+var PORT = 8080;
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
+var http = require('http');
+var fs   = require('fs');
+var path = require('path');
+var mime = require('mime');
 
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index');
-})
+var app = http.createServer(function(req, res) {
+        var fPath = false;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+        if (req.url == '/') {
+                fPath = 'public/index.html';
+        } else {
+                fPath = 'public' + req.url;
+        }
+
+        var absPath = './' + fPath;
+        serveStatic(res, absPath);
+});
+
+app.listen(PORT, function() {
+        console.log('Server listening on port ' + PORT);
+});
+
+function serveStatic(res, fPath) {
+        fs.exists(fPath, function(exists) {
+                if (exists) {
+                        fs.readFile(fPath, function(err, data) {
+                                if (err) {
+                                        send404(res);
+                                } else {
+                                        sendFile(res, fPath, data);
+                                }
+                        });
+                } else {
+                        send404(res);
+                }
+        });
+}
+
+function send404(res) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404: resource not found.');
+        res.end();
+}
+
+function sendFile(res, fPath, contents) {
+        res.writeHead(200, {'content-type': mime.lookup(path.basename(fPath))});
+        res.end(contents);
+}
+
+var udpServer = require('./lib/udp_server');
+udpServer.listen(app);
