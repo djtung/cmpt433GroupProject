ea61395f92abc17e781abd9201188495770f499e
Matthew <tsengmatthew@hotmail.com>
2018-07-27 21:58:10 -0700

    fixed nodejs and still fixing packets for communication

--- networkModule.h
+++ networkModule.h
@@ -9,11 +9,14 @@
  * Initializes the networking module and thread
  * @return 0 if successful, otherwise error
  */
-int network_init(void);
+int NM_init(void);
 
 /**
  * Cleans up the module and destroys the associated thread
  */
-void network_cleanup(void);
+void NM_cleanup(void);
+
+static int NM_buildStatusMessage(char *buf);
+static void NM_sendMessage(struct sockaddr_in sa, int fd, char *buf, size_t msg_size);
 
 #endif
